01d1ac5e64eae545c0edc968a1faa2c59a729018
임빛나 <devbit4@gmail.com>
2022-09-20 01:48:45 +0900

    house drawing

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = 800;
+canvas.height = 800;
 /**
  * rect
  */
-// canvas.width = 800;
-// canvas.height = 800;
 
 // ctx.rect(50, 50, 100, 100);
 // ctx.rect(150, 150, 100, 100);
@@ -21,12 +21,31 @@ const ctx = canvas.getContext('2d');
 /**
  * heart
  */
+// ctx.beginPath();
+// ctx.moveTo(75, 40);
+// ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+// ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+// ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+// ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+// ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+// ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+// ctx.fill();
+
+/**
+ * house
+ */
+
+ctx.fillRect(200, 200, 20, 200);
+ctx.fillRect(400, 200, 20, 200);
+ctx.lineWidth = 3;
+ctx.strokeStyle = 'yellow';
+ctx.strokeRect(290, 300, 40, 100);
+ctx.fillRect(200, 200, 200, 20);
+
 ctx.beginPath();
-ctx.moveTo(75, 40);
-ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+ctx.lineWidth = 3;
+ctx.fillStyle = 'red';
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(420, 200);
 ctx.fill();
