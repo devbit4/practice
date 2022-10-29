5cbe723870b58ed6e6a4d5a3061e83d31051ac70
임빛나 <devbit4@gmail.com>
2022-10-29 14:39:50 +0900

    canvas challenge-background colorpicker

--- challenge-canvas3/index.js
+++ challenge-canvas3/index.js
@@ -1,15 +1,19 @@
 const canvas = document.querySelector('canvas');
-const color = document.getElementById('color');
+const color = document.querySelector('#color');
 const colorOptions = Array.from(document.querySelectorAll('.color-option'));
-const fill = document.getElementById('fill');
-const stroke = document.getElementById('stroke');
+const fill = document.querySelector('#fill');
+const stroke = document.querySelector('#stroke');
+const modeBtn = document.querySelector('#mode-btn');
+
 const ctx = canvas.getContext('2d');
+
 canvas.width = 600;
 canvas.height = 600;
 
 ctx.lineWidth = 5;
 let isPainting = false;
 let isFilling = false;
+let isBgFilling = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -32,17 +36,12 @@ function cancelPainting() {
   }
 }
 
+/**
+ * 브러쉬 색 변경
+ */
 function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
-}
-
-function onFillClick() {
-  isFilling = true;
-}
-
-function onStrokeClick() {
-  isFilling = false;
 }
 
 function onColorClick(event) {
@@ -52,16 +51,51 @@ function onColorClick(event) {
   color.value = colorValue;
 }
 
+/**
+ * 브러쉬 타입 변경
+ */
+function onFillClick() {
+  isFilling = true;
+}
+
+function onStrokeClick() {
+  isFilling = false;
+}
+
+/**
+ * 배경 색 변경
+ */
+function onModeClick() {
+  if (isBgFilling) {
+    isBgFilling = false;
+    modeBtn.innerText = 'BackgroundFill';
+  } else {
+    isBgFilling = true;
+    modeBtn.innerText = 'BackgroundDraw';
+  }
+}
+
+function onCanvasClick() {
+  if (isBgFilling) {
+    ctx.fillRect(0, 0, 600, 600);
+  }
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
 
-// 브러쉬
+// 브러쉬 색
 color.addEventListener('change', onColorChange);
-fill.addEventListener('click', onFillClick);
-stroke.addEventListener('click', onStrokeClick);
-
 colorOptions.forEach((color) => {
   color.addEventListener('click', onColorClick);
 });
+
+// 브러쉬 타입
+fill.addEventListener('click', onFillClick);
+stroke.addEventListener('click', onStrokeClick);
+
+// 배경
+modeBtn.addEventListener('click', onModeClick);
+canvas.addEventListener('click', onCanvasClick);
