8cd03ac2f2b43fd126cfbb4d6e235f52edd244c5
임빛나 <devbit4@gmail.com>
2022-10-29 14:52:45 +0900

    canvas challenge-brush and background eraser

--- challenge-canvas3/index.js
+++ challenge-canvas3/index.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector('canvas');
 const color = document.querySelector('#color');
 const colorOptions = Array.from(document.querySelectorAll('.color-option'));
-const fill = document.querySelector('#fill');
-const stroke = document.querySelector('#stroke');
+const fillBtn = document.querySelector('#fill-btn');
+const strokeBtn = document.querySelector('#stroke-btn');
 const modeBtn = document.querySelector('#mode-btn');
+const eraseBtn = document.querySelector('#eraser-btn');
+const destroyBtn = document.querySelector('#destroy-btn');
 
 const ctx = canvas.getContext('2d');
 
@@ -81,6 +83,17 @@ function onCanvasClick() {
   }
 }
 
+function onEraserClick() {
+  ctx.strokeStyle = 'white';
+  isBgFilling = false;
+  modeBtn.innerText = 'BackgroundFill';
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, 600, 600);
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
@@ -93,9 +106,13 @@ colorOptions.forEach((color) => {
 });
 
 // 브러쉬 타입
-fill.addEventListener('click', onFillClick);
-stroke.addEventListener('click', onStrokeClick);
+fillBtn.addEventListener('click', onFillClick);
+strokeBtn.addEventListener('click', onStrokeClick);
 
 // 배경
 modeBtn.addEventListener('click', onModeClick);
 canvas.addEventListener('click', onCanvasClick);
+
+// 지우개
+eraseBtn.addEventListener('click', onEraserClick);
+destroyBtn.addEventListener('click', onDestroyClick);
